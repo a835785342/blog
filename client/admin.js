b47873c92e3925bda997a23d90c411f31fa1b6c2
Feel <835785342@qq.com>
2019-03-03 21:49:41 +0800

    1、更新管理后台css、js文件

--- client/admin.js
+++ client/admin.js
@@ -2,77 +2,78 @@ import {Template} from 'meteor/templating';
 import moment from "moment";
 import {Meteor} from "meteor/meteor";
 import {topics} from "../collections/topic";
+import '../static/css/bootstrap-table.min.css';
+import '../static/js/bootstrap-table.min';
+import '../static/js/bootstrap-table-zh-CN.min';
 
 Template.adminLogin.events({
-    'click .adminLoginBtn':function (event) {
+    'click .adminLoginBtn': function (event) {
         event.preventDefault();
-        const userName=$('#inputUser').val();
-        const password=$('#inputPassword').val();
-        if(userName=='admin'&&password=='admin'){
-            Session.set('isAdminLogin',1);
-            Session.set('isAdminUser',1);
-            Session.set('isAdminBlog',0);
-            Session.set('isAdminTopic',0);
-        }else{
+        const userName = $('#inputUser').val();
+        const password = $('#inputPassword').val();
+        if (userName == 'admin' && password == 'admin') {
+            Session.set('isAdminLogin', 1);
+            Session.set('isAdminUser', 1);
+            Session.set('isAdminBlog', 0);
+            Session.set('isAdminTopic', 0);
+        } else {
             alert('密码错误！');
         }
     }
 });
 
 Template.admin.events({
-    'click .adminLogout':function () {
-        Session.set('isAdminLogin',0);
+    'click .adminLogout': function () {
+        Session.set('isAdminLogin', 0);
     },
-    'click .adminBlog':function () {
-        Session.set('isAdminUser',0);
-        Session.set('isAdminBlog',1);
-        Session.set('isAdminTopic',0);
+    'click .adminBlog': function () {
+        Session.set('isAdminUser', 0);
+        Session.set('isAdminBlog', 1);
+        Session.set('isAdminTopic', 0);
     },
-    'click .adminTopic':function () {
-        Session.set('isAdminUser',0);
-        Session.set('isAdminBlog',0);
-        Session.set('isAdminTopic',1);
+    'click .adminTopic': function () {
+        Session.set('isAdminUser', 0);
+        Session.set('isAdminBlog', 0);
+        Session.set('isAdminTopic', 1);
     },
-    'click .adminUser':function () {
-        Session.set('isAdminUser',1);
-        Session.set('isAdminBlog',0);
-        Session.set('isAdminTopic',0);
+    'click .adminUser': function () {
+        Session.set('isAdminUser', 1);
+        Session.set('isAdminBlog', 0);
+        Session.set('isAdminTopic', 0);
     }
 
 });
 
 //用户管理
 Template.adminUser.helpers({
-   allUser(){
-       // Meteor.subscribe("users");
-       // console.log(Meteor.users.findOne());
-       return Meteor.users.find().fetch();
-   },
-    userStatus(status){
-        return status=="normal";
+    allUser() {
+        return Meteor.users.find().fetch();
+    },
+    userStatus(status) {
+        return status == "normal";
     }
 
 });
 
 Template.adminUser.events({
-    "click .deleteUser":function (event) {
-        const userID=$(event.currentTarget).data("id");
-        Meteor.users.remove({_id:userID});
+    "click .deleteUser": function (event) {
+        const userID = $(event.currentTarget).data("id");
+        Meteor.users.remove({_id: userID});
     },
-    "click .closeUser":function (event) {
-        const userID=$(event.currentTarget).data("id");
-        Meteor.users.update({_id:userID},{$set:{"profile.status":"closed"}});
+    "click .closeUser": function (event) {
+        const userID = $(event.currentTarget).data("id");
+        Meteor.users.update({_id: userID}, {$set: {"profile.status": "closed"}});
     },
-    "click .uncloseUser":function (event) {
-        const userID=$(event.currentTarget).data("id");
-        Meteor.users.update({_id:userID},{$set:{"profile.status":"normal"}});
+    "click .uncloseUser": function (event) {
+        const userID = $(event.currentTarget).data("id");
+        Meteor.users.update({_id: userID}, {$set: {"profile.status": "normal"}});
     }
 });
 //用户管理结束
 //微博管理
 Template.adminBlog.helpers({
-    allBlog(){
-        let blog=blogs.find().fetch();
+    allBlog() {
+        let blog = blogs.find().fetch();
         for (let i = 0, len = blog.length; i < len; i++) {
             blog[i].createTime = moment(blog[i].createTime).format("YYYY年MM月DD日 HH:mm");
         }
@@ -82,22 +83,22 @@ Template.adminBlog.helpers({
 
 });
 Template.adminBlog.events({
-    "click .deleteBlog":function (event) {
-        const blogID=$(event.currentTarget).data("id");
-        blogs.remove({_id:blogID});
+    "click .deleteBlog": function (event) {
+        const blogID = $(event.currentTarget).data("id");
+        blogs.remove({_id: blogID});
     }
 });
 //微博管理结束
 //话题管理
 Template.adminTopic.helpers({
-    allTopic(){
-        let topic=topics.find().fetch();
+    allTopic() {
+        let topic = topics.find().fetch();
         for (let i = 0, len = topic.length; i < len; i++) {
             topic[i].createTime = moment(topic[i].createTime).format("YYYY年MM月DD日 HH:mm");
         }
         return topic;
     },
-    allCandidatePerson(){
+    allCandidatePerson() {
         return Session.get('allCandidatePerson');
     }
 
@@ -105,31 +106,34 @@ Template.adminTopic.helpers({
 });
 
 Template.adminTopic.events({
-    "click .deleteTopic":function (event) {
-        const topicID=$(event.currentTarget).data("id");
-        let users=Meteor.users.find({'profile.followTopics':{"$in":[topicID]}}).fetch();
-        for(let i=0;i<users.length;i++){
-            Meteor.users.update({_id:users[i]._id},{$pull:{"profile.followTopics":topicID}});
+    "click .deleteTopic": function (event) {
+        const topicID = $(event.currentTarget).data("id");
+        let users = Meteor.users.find({'profile.followTopics': {"$in": [topicID]}}).fetch();
+        for (let i = 0; i < users.length; i++) {
+            Meteor.users.update({_id: users[i]._id}, {$pull: {"profile.followTopics": topicID}});
         }
-        topics.remove({_id:topicID});
+        topics.remove({_id: topicID});
     },
-    'click .catHostCandidatePerson':function (event) {
-        const topicID=$(event.currentTarget).data("id");
-        const allCandidatePerson=topics.findOne({_id:topicID}).candidatePerson;
-        for(let i=0;i<allCandidatePerson.length;i++){
-            allCandidatePerson[i]={id:allCandidatePerson[i],username:Meteor.users.findOne({_id:allCandidatePerson[i]}).username};
+    'click .catHostCandidatePerson': function (event) {
+        const topicID = $(event.currentTarget).data("id");
+        const allCandidatePerson = topics.findOne({_id: topicID}).candidatePerson;
+        for (let i = 0; i < allCandidatePerson.length; i++) {
+            allCandidatePerson[i] = {
+                id: allCandidatePerson[i],
+                username: Meteor.users.findOne({_id: allCandidatePerson[i]}).username
+            };
 
         }
-        Session.set('topicID',topicID);
-        Session.set('allCandidatePerson',allCandidatePerson);
+        Session.set('topicID', topicID);
+        Session.set('allCandidatePerson', allCandidatePerson);
 
     },
-    'click .approveCandidatePerson':function (event) {
-        const candidatePersonID=$(event.currentTarget).data("id");
-        const username=Meteor.users.findOne({_id:candidatePersonID}).username;
-        const topicID=Session.get('topicID');
-        topics.update({_id:topicID},{$set:{'hostID':candidatePersonID,'hostName':username}});
-        topics.update({_id:topicID},{$pull:{candidatePerson:candidatePersonID}});
+    'click .approveCandidatePerson': function (event) {
+        const candidatePersonID = $(event.currentTarget).data("id");
+        const username = Meteor.users.findOne({_id: candidatePersonID}).username;
+        const topicID = Session.get('topicID');
+        topics.update({_id: topicID}, {$set: {'hostID': candidatePersonID, 'hostName': username}});
+        topics.update({_id: topicID}, {$pull: {candidatePerson: candidatePersonID}});
         $('#modalCandidatePerson').modal('hide')
     },
 
--- routers/router.js
+++ routers/router.js
@@ -3,65 +3,69 @@ import '../client/personalCenter.html';
 import '../client/search.html';
 import '../client/topic.html';
 import '../client/admin.html';
+import {Meteor} from "meteor/meteor";
+
 Router.configure({
-    layoutTemplate:'masterLayout'
+    layoutTemplate: 'masterLayout'
 });
 
 Router.route('/personalCenter/:_id', function () {
-    this.render("personalCenter",{
-        data:function () {
-            return Meteor.users.findOne({_id:this.params._id});
+    this.render("personalCenter", {
+        data: function () {
+            return Meteor.users.findOne({_id: this.params._id});
         }
     });
-    this.render("header",{
-        to:'personalInformationHead',
-        data:function () {
-            return Meteor.users.findOne({_id:this.params._id});
+    this.render("header", {
+        to: 'personalInformationHead',
+        data: function () {
+            return Meteor.users.findOne({_id: this.params._id});
         }
     });
-    this.render("left",{
-        to:"left"
+    this.render("left", {
+        to: "left"
     });
-},{
-    name:'personalCenter'
+}, {
+    name: 'personalCenter'
 });
 
-Router.route('/',function () {
+Router.route('/', function () {
     this.render("index");
-    this.render("left",{
-        to:"left"
+    this.render("left", {
+        to: "left"
     });
 });
 
-Router.route('/search/:searchContext',function () {
+Router.route('/search/:searchContext', function () {
     this.render("searchContext");
 });
 
-Router.route('/search/',function () {
+Router.route('/search/', function () {
     this.render("searchContext");
 });
 
-Router.route('/topic/',function () {
-   this.render('topic');
+Router.route('/topic/', function () {
+    this.render('topic');
 });
 
-Router.route('/topic/:topicID',function () {
+Router.route('/topic/:topicID', function () {
     this.render("topicItem");
 });
 
-Router.route('/admin/',function () {
+Router.route('/admin/', function () {
     this.layout('');
-    if(Session.equals('isAdminLogin',1)){
+    if (Session.equals('isAdminLogin', 1)) {
         this.layout('admin');
-        if(Session.equals('isAdminUser',1)){
+        if (Session.equals('isAdminUser', 1)) {
             this.render('adminUser');
-        }else if(Session.equals('isAdminBlog',1)){
+        } else if (Session.equals('isAdminBlog', 1)) {
             this.render('adminBlog');
-        }else if(Session.equals('isAdminTopic',1)){
+        } else if (Session.equals('isAdminTopic', 1)) {
             this.render('adminTopic');
         }
-    }else{
+    } else {
         this.render('adminLogin');
     }
 
 });
+
+
